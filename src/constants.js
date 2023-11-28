4271369ceda0af64af66f219dc49c48e4f475b0e
liumen <91662136+mytom2trung@users.noreply.github.com>
2023-11-28 22:23:47 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const mainsite = "https://animesuborg.vercel.app/"
+const mainsite = "https://animesuborg.vercel.app"
 // const mainsite = "http://localhost:3000"
 // const url = "http://localhost:3000/api"
 const url = "https://mirai-backend.onrender.com/api"
